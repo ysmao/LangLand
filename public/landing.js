8225038e47abf817f2b2c58b087252ed7f9888d1
rfding <rita_ding@brown.edu>
2018-04-20 15:47:26 -0400

    removed alerts when signing up and logging in (was for testing)

--- public/landing.js
+++ public/landing.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	$('#signup_form').submit(function(event) {
 		event.preventDefault();
-		alert('signup submitted');
 
 		var username = $('#signup_username').val();
 	    var password = $('#signup_password').val();
@@ -17,7 +16,6 @@ $(document).ready(function() {
 
 	$('#login_container').submit(function(event) {
 		event.preventDefault();
-		alert('login submitted');
 
 		var username = $('#login_username').val();
 	    var password = $('#login_password').val();
