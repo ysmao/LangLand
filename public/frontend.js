509ebc0caaa99d975b6cdd224d2ce7cc0f842386
bmora97 <beatriz_mora@brown.edu>
2018-04-19 21:07:08 -0400

    finally the frontend

---
+++ public/frontend.js
@@ -0,0 +1,45 @@
+
+function toggleChatsTab() {
+	$('#chats_tab_content').show();
+	$('#friends_tab_content').hide();
+
+	$('#chats_tab').addClass("selected");
+	$('#chats_tab_content').addClass("selected");
+
+	$('#friends_tab').removeClass("selected");
+	$('#friends_tab_content').removeClass("selected");
+}
+
+function toggleFriendsTab() {
+	$('#chats_tab_content').hide();
+	$('#friends_tab_content').show();
+
+	$('#friends_tab').addClass("selected");
+	$('#friends_tab_content').addClass("selected");
+
+	$('#chats_tab').removeClass("selected");
+	$('#chats_tab_content').removeClass("selected");
+}
+
+function displayLogin() {
+	$('#login_container').css('display', 'inline-flex');
+	$('#landing_buttons').hide();
+}
+
+function displaySignupOne() {
+	$('#signup_pt_one').css('display', 'inline-flex');
+	$('#landing_buttons').hide();
+	$('#signup_pt_two').hide();
+
+	$('#steps').show();
+	$('#step_one').addClass("current");
+	$('#step_two').removeClass("current");
+}
+
+function displaySignupTwo() {
+	$('#signup_pt_two').css('display', 'inline-flex');
+	$('#signup_pt_one').hide();
+
+	$('#step_two').addClass("current");
+	$('#step_one').removeClass("current");
+}
