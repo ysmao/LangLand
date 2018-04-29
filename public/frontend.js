aff2361732cef9fb1f9f0e628e31e9858bcd1fdd
bmora97 <beatriz_mora@brown.edu>
2018-04-29 17:38:08 -0400

    fixed the landing functions lol my bad

--- public/frontend.js
+++ public/frontend.js
@@ -8,27 +8,4 @@ function toggleFriendsTab() {
 	window.location = '/friends';
 }
 
-function displayLogin() {
-	$('#login_container').css('display', 'inline-flex');
-	$('#landing_buttons').hide();
-}
-
-function displaySignupOne() {
-	$('#signup_pt_one').css('display', 'inline-flex');
-	$('#landing_buttons').hide();
-	$('#signup_pt_two').hide();
-
-	$('#steps').show();
-	$('#step_one').addClass("current_step");
-	$('#step_two').removeClass("current_step");
-}
-
-function displaySignupTwo() {
-	$('#signup_pt_two').css('display', 'inline-flex');
-	$('#signup_pt_one').hide();
-
-	$('#step_two').addClass("current_step");
-	$('#step_one').removeClass("current_step");
-}
-
 console.log(window.location.href);
