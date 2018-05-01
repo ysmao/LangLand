16df32b9c172610740a4fa35e3f106edf8521fb7
rfding <rita_ding@brown.edu>
2018-04-30 21:41:57 -0400

    message list auto scrolls to bottom

--- public/chat.js
+++ public/chat.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
 	var pathname = window.location.pathname.split( '/' );
 	var otherPerson = pathname[2];
 
+    $('#message_box').focus();
+
 	socket.emit("join", me, function(something) {
 		console.log(something);
 	});
@@ -51,30 +53,33 @@ $(document).ready(function() {
     			console.log(val);
     		});
 
+    		$('#message_box').focus();
     	});
 	});
 
 	$('#new_message').submit(function(event) {
 		if(editing===false){
-		event.preventDefault();
+			event.preventDefault();
 
-		var pathname = window.location.pathname.split( '/' );
+			var pathname = window.location.pathname.split( '/' );
 
-		var message = $('#message_box').val();
-		var time = new Date().getTime();
-		var sender = $('#message_sender').val();
-		var receiver = pathname[2];
-		
-		$('#new_message')[0].reset(); //go back to default
-    	// $.post('/chats/save', {message:message, time:time, sender:sender, receiver:receiver}, function(res){
-     //    	//you might want to add callback function that is executed post request success
-     //    	console.log('msg sent');
-    	// });
+			var message = $('#message_box').val();
+			var time = new Date().getTime();
+			var sender = $('#message_sender').val();
+			var receiver = pathname[2];
+			
+			$('#new_message')[0].reset(); //go back to default
+	    	// $.post('/chats/save', {message:message, time:time, sender:sender, receiver:receiver}, function(res){
+	     //    	//you might want to add callback function that is executed post request success
+	     //    	console.log('msg sent');
+	    	// });
 
-		socket.emit('message', {message:message, time:time, sender:sender, receiver:receiver}, function(val) {
-			console.log(val);
-		});
-}
+			socket.emit('message', {message:message, time:time, sender:sender, receiver:receiver}, function(val) {
+				console.log(val);
+			});
+
+    		$('#message_box').focus();
+		}
 	});
 
 	$('.correct').click(function(event){
@@ -102,18 +107,31 @@ $(document).ready(function() {
 	//         }
 	//     });
 	// });
+
+	var chat = document.getElementById("chat_display");
+	chat.scrollTop = chat.scrollHeight - chat.clientHeight;
 });
 
 function addMyMessage(val) {
+	var chat = document.getElementById("chat_display");
+	var isScrolledToBottom = chat.scrollHeight - chat.clientHeight <= chat.scrollTop + 1;
+
 	var first_li = '<li class="message my_message">';
 	var msg_content = '<div class="message_content">' + val.message + '</div>';
 
 	var rendered_message = first_li + msg_content + '</li>';
 
 	$('#messages_list').append(rendered_message);
+
+	if(isScrolledToBottom) {
+	    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+	}
 }
 
 function addYourMessage(val) {
+	var chat = document.getElementById("chat_display");
+	var isScrolledToBottom = chat.scrollHeight - chat.clientHeight <= chat.scrollTop + 1;
+
 	var first_li = '<li class="message your_message">';
 	var img = '<img src="/placeholder.png" alt="' + val.sender + '" class="avatar">';
 	var msg_content = '<div class="message_content">' + val.message + '</div>';
@@ -124,4 +142,8 @@ function addYourMessage(val) {
 	var rendered_message = first_li + img + msg_content + edit_button + '</li>';
 
 	$('#messages_list').append(rendered_message);
+
+	if(isScrolledToBottom) {
+	    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+	}
 }
